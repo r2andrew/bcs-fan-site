592f49f471b64dd7d7976ca83b18bfc21f58ce4c
r2andrew <abradley4646@gmail.com>
2024-11-01 16:24:57 +0000

    register route

--- blueprints/auth/auth.py
+++ blueprints/auth/auth.py
@@ -5,11 +5,48 @@ from decorators import jwt_required
 import bcrypt
 import jwt
 import datetime
+from bson import ObjectId
+import re
 
 auth_bp = Blueprint("auth_bp", __name__)
 
 users = globals.db.users
 blacklist = globals.db.blacklist
+
+@auth_bp.route("/api/v1.0/register", methods=["POST"])
+def register():
+    if "name" in request.form and "username" in request.form and "password" in request.form and "email" in request.form:
+        if re.match(r"[^@]+@[^@]+\.[^@]+", request.form["email"]):
+            try:
+                existing_user = users.find_one({'username': request.form["username"]})
+            except pymongo.errors.ServerSelectionTimeoutError:
+                return make_response(jsonify({"error": "Connection to database timed out"}), 500)
+            except pymongo.errors:
+                return make_response(jsonify({"error": "An unknown error occurred in the database"}), 500)
+            if not existing_user:
+                password = request.form["password"].encode("utf-8")
+                new_user = {
+                    "_id" : ObjectId(),
+                    "name": str(request.form["name"]),
+                    "username" : str(request.form["username"]),
+                    "password" : bcrypt.hashpw(password, bcrypt.gensalt()),
+                    "email" : str(request.form["email"]),
+                    "admin": False
+                }
+                try:
+                    users.insert_one(new_user)
+                except pymongo.errors.ServerSelectionTimeoutError:
+                    return make_response(jsonify({"error": "Connection to database timed out"}), 500)
+                except:
+                    return make_response(jsonify({"error": "An unknown error occurred in the database"}), 500)
+
+                return make_response(jsonify({"message": "User created"}), 201)
+            else:
+                return make_response(jsonify({"message": "Username taken"}), 409)
+        else:
+            return make_response(jsonify({"error": "Invalid email format"}), 422)
+    else:
+        return make_response( jsonify({ "error" : "Missing form data" } ), 404)
 
 @auth_bp.route('/api/v1.0/login', methods=['GET'])
 def login():
@@ -43,7 +80,7 @@ def login():
 
 @auth_bp.route('/api/v1.0/logout', methods=["GET"])
 @jwt_required
-def logout():
+def logout(token):
     token = request.headers['x-access-token']
     try:
         blacklist.insert_one({"token":token})
