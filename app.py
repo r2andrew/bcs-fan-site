10cea065ed2b1a130dd0831d981af78a19622ea7
r2andrew <abradley4646@gmail.com>
2024-11-02 20:31:00 +0000

    add get_user endpoint

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from blueprints.episodes.episodes import episodes_bp
 from blueprints.trivia.trivia import trivias_bp
+from blueprints.users.users import users_bp
 from blueprints.auth.auth import auth_bp
 app = Flask(__name__)
 app.register_blueprint(episodes_bp)
 app.register_blueprint(trivias_bp)
+
+app.register_blueprint(users_bp)
 app.register_blueprint(auth_bp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ blueprints/users/users.py
@@ -0,0 +1,24 @@
+import string
+from flask import Blueprint, request, make_response, jsonify
+import globals
+import pymongo.errors
+from bson import ObjectId
+
+users_bp = Blueprint("users_bp", __name__)
+
+users = globals.db.users
+
+@users_bp.route("/api/v1.0/users/<string:uid>", methods=["GET"])
+def get_user(uid):
+    if not all(c in string.hexdigits for c in uid):
+        return make_response(jsonify({"error" : "Invalid id format"} ), 422)
+    try:
+        user = users.find_one({'_id': ObjectId(uid)}, {"password":0, "_id": 0})
+    except pymongo.errors.ServerSelectionTimeoutError:
+        return make_response(jsonify({"error" : "Connection to database timed out"} ), 500)
+    # except:
+    #     return make_response(jsonify({"error" : "An unknown error occurred in the database"}), 500)
+    if user is not None:
+        return make_response( jsonify( user ), 200 )
+    else:
+        return make_response( jsonify({"error" : "Invalid episode ID"} ), 404 )
