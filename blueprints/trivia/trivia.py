0740700685e4a85aa881619be77220c63346b4d1
r2andrew <abradley4646@gmail.com>
2024-11-02 16:37:04 +0000

    score returned from get one trivia

--- blueprints/trivia/trivia.py
+++ blueprints/trivia/trivia.py
@@ -42,10 +42,7 @@ def fetch_all_trivias(id):
         return make_response(jsonify({"error" : "Invalid id format"} ), 422)
     data_to_return = []
 
-    stages = [
-        {
-            "$match" : {"_id" : ObjectId(id)}
-        },
+    stages = [ { "$match" : {"_id" : ObjectId(id)} },
         {
             "$addFields": {
                 "trivias": {
@@ -59,8 +56,7 @@ def fetch_all_trivias(id):
                                     "score": {"$subtract" : [
                                         { "$cond": { "if": { "$isArray": "$$item.upvotes" }, "then": { "$size": "$$item.upvotes" }, "else": 0}},
                                         { "$cond": { "if": { "$isArray": "$$item.downvotes" }, "then": { "$size": "$$item.downvotes" }, "else": 0}}
-                                    ]
-                                    }
+                                    ] }
                                 }
                             ]
                         }
@@ -69,7 +65,6 @@ def fetch_all_trivias(id):
             }
         }
     ]
-
     try:
         episode = list(episodes.aggregate(stages))
     except pymongo.errors.ServerSelectionTimeoutError:
@@ -87,17 +82,33 @@ def fetch_one_trivia(eid, tid):
         return make_response(jsonify({"error" : "Invalid episode id format"} ), 422)
     if not all(c in string.hexdigits for c in tid):
         return make_response(jsonify({"error" : "Invalid trivia id format"} ), 422)
+
+    stages = [{"$match": {"_id": ObjectId(eid)}},
+              {"$unwind" : "$trivias"},
+              {"$match" : {"trivias._id": ObjectId(tid)}},
+              {"$project" : {"trivias" : 1, "_id" : 0}},
+              {
+                  "$addFields" : {
+                      "trivias.score": {"$subtract": [
+                          {"$cond": {"if": {"$isArray": "$trivias.upvotes"},
+                                     "then": {"$size": "$trivias.upvotes"}, "else": 0}},
+                          {"$cond": {"if": {"$isArray": "$trivias.downvotes"},
+                                     "then": {"$size": "$trivias.downvotes"}, "else": 0}}
+                      ]}
+                  }
+              }
+              ]
     try:
-        episode = episodes.find_one({ "trivias._id" : ObjectId(tid) },
-        { "_id" : 0, "trivias.$" : 1 } )
+        episode = list(episodes.aggregate(stages))
     except pymongo.errors.ServerSelectionTimeoutError:
         return make_response(jsonify({"error": "Connection to database timed out"}), 500)
     except:
         return make_response(jsonify({"error": "An unknown error occurred in the database"}), 500)
     if episode is None:
         return make_response(jsonify({"error":"Invalid episode ID or trivia ID"}),404)
-    episode['trivias'][0]['_id'] = str(episode['trivias'][0]['_id'])
-    return make_response( jsonify(episode['trivias'][0]), 200)
+    print(episode[0]["trivias"])
+    episode[0]["trivias"]['_id'] = str(episode[0]['trivias']['_id'])
+    return make_response( jsonify(episode[0]['trivias']), 200)
 
 @trivias_bp.route("/api/v1.0/episodes/<eid>/trivias/<tid>", methods=["PATCH"])
 @jwt_required
@@ -123,9 +134,9 @@ def edit_trivia(token, eid, tid):
         return make_response(jsonify({"error": "Missing form data"}), 404)
 
 
-@trivias_bp.route("/api/v1.0/episodes/<string:id>/trivias/<tid>/vote", methods=["PATCH"])
+@trivias_bp.route("/api/v1.0/episodes/<string:eid>/trivias/<tid>/vote", methods=["PATCH"])
 @jwt_required
-def vote_on_trivia(token, id, tid):
+def vote_on_trivia(token, eid, tid):
     if not all(c in string.hexdigits for c in id):
         return make_response(jsonify({"error" : "Invalid id format"} ), 422)
     if request.args.get('vote') == "up":
@@ -151,7 +162,9 @@ def vote_on_trivia(token, id, tid):
         return make_response(jsonify({"error": "Connection to database timed out"}), 500)
     except:
         return make_response(jsonify({"error": "An unknown error occurred in the database"}), 500)
-    return make_response( jsonify( { "message" : "Vote recorded" } ), 200 )
+    edited_trivia_url = "http://localhost:5000/api/v1.0/episodes/" + \
+        eid + "/trivias/" + tid
+    return make_response( jsonify( { "url" : edited_trivia_url } ), 200 )
 
 @trivias_bp.route("/api/v1.0/episodes/<eid>/trivias/<tid>", methods=["DELETE"])
 @jwt_required
