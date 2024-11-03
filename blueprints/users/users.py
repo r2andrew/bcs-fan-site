70b63e3dc48d359c80be4f66eaccbeee6e6718c9
r2andrew <abradley4646@gmail.com>
2024-11-03 11:42:42 +0000

    user id lookup to username

--- blueprints/users/users.py
+++ blueprints/users/users.py
@@ -1,19 +1,15 @@
-import string
 from flask import Blueprint, request, make_response, jsonify
 import globals
 import pymongo.errors
-from bson import ObjectId
 
 users_bp = Blueprint("users_bp", __name__)
 
 users = globals.db.users
 
-@users_bp.route("/api/v1.0/users/<string:uid>", methods=["GET"])
-def get_user(uid):
-    if not all(c in string.hexdigits for c in uid):
-        return make_response(jsonify({"error" : "Invalid id format"} ), 422)
+@users_bp.route("/api/v1.0/users/<string:username>", methods=["GET"])
+def get_user(username):
     try:
-        user = users.find_one({'_id': ObjectId(uid)}, {"password":0, "_id": 0})
+        user = users.find_one({'username': username}, {"password":0, "_id": 0})
     except pymongo.errors.ServerSelectionTimeoutError:
         return make_response(jsonify({"error" : "Connection to database timed out"} ), 500)
     except:
