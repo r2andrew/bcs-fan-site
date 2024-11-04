0620af349c30aae68ff65c218dbd3ef4c23c53c5
r2andrew <abradley4646@gmail.com>
2024-11-04 10:31:38 +0000

    return id

--- blueprints/users/users.py
+++ blueprints/users/users.py
@@ -9,12 +9,13 @@ users = globals.db.users
 @users_bp.route("/api/v1.0/users/<string:username>", methods=["GET"])
 def get_user(username):
     try:
-        user = users.find_one({'username': username}, {"password":0, "_id": 0})
+        user = users.find_one({'username': username}, {"password":0})
     except pymongo.errors.ServerSelectionTimeoutError:
         return make_response(jsonify({"error" : "Connection to database timed out"} ), 500)
     except:
         return make_response(jsonify({"error" : "An unknown error occurred in the database"}), 500)
     if user is not None:
+        user["_id"] = str(user["_id"])
         return make_response( jsonify( user ), 200 )
     else:
         return make_response( jsonify({"error" : "Invalid episode ID"} ), 404 )
